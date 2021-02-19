b1d749a77222997ac49800336e1dd5d324d5c589
kraj09 <49805482+kraj09@users.noreply.github.com>
2021-02-19 07:37:48 +0530

    Project Created

---
+++ MobileTest.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MobileTest.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ MobileTest.IoC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MobileTest.IoC
+{
+    public class Class1
+    {
+    }
+}
